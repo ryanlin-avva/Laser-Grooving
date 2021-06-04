c9f741c09745c9630280b10e6fdd4e907878a3d5
ryan_lin <ryan_lin@avvatech.com>
2021-06-04 17:24:55 +0800

    加長INTL_DataSaveDelay至10秒，加快對焦速度，GoHome後將hasGoHome設為true

---
+++ MyForm/WaferCenterPointForm.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace Velociraptor
+{
+    partial class WaferCenterPointForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("標楷體", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(-6, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(473, 185);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "請放置晶圓後\r\n將晶圓切割道對齊紅光雷射\r\n後移至晶圓中心對焦\r\n對焦完成後\r\n請按下紀錄中心點";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            this.button1.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.button1.Font = new System.Drawing.Font("標楷體", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button1.Location = new System.Drawing.Point(148, 212);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(167, 47);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "下一步";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // WaferCenterPointForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(470, 281);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "WaferCenterPointForm";
+            this.Text = "WaferCenterPointForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+    }
+}
